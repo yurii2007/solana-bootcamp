a902f219f20b969085f98af78cc91f7a7bdf86db
yurii2007 <yurash862@gmail.com>
2024-03-12 20:04:08 +0200

    added possibility to read the file

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
-use std::env;
+use std::{env, fs};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     
     let query = &args[1];
     let filename = &args[2];
+
+    let content = fs::read_to_string(filename).expect("Should be able to reed the file");
+
+    println!("{content}");
 }
