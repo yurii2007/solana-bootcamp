e97f622636c19e7e828d1c9ba4163a87f1209b5c
yurii2007 <yurash862@gmail.com>
2024-03-13 19:59:07 +0200

    separate parsing arguments from main

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,32 @@
-use std::{env, fs};
+use std::{env, fs, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    
-    let query = &args[1];
-    let filename = &args[2];
 
-    let content = fs::read_to_string(filename).expect("Should be able to reed the file");
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem with parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    let content = fs::read_to_string(config.file_path).expect("Should be able to reed the file");
 
     println!("{content}");
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("insufficient arguments");
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config { query, file_path })
+    }
 }
