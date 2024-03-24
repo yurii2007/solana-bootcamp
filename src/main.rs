18ff703f8a27330740056cb07cc3ccac636d215c
yurii2007 <yurash862@gmail.com>
2024-03-24 20:34:19 +0200

    improved error handling

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem with parsing arguments: {err}");
+        eprintln!("Problem with parsing arguments: {err}");
         process::exit(1);
     });
 
     if let Err(e) = run(config) {
-        println!("application error: {e}");
+        eprintln!("application error: {e}");
         process::exit(1);
     };
 }
